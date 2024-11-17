002ff7fa8c89d03f7058f6066e838c74fa98669e
Rohit Kumar <67968330+iCriptSnappie@users.noreply.github.com>
2024-11-17 12:27:12 +0530

    dd

--- assets/js/script.js
+++ assets/js/script.js
@@ -106,7 +106,7 @@ $(document).ready(function () {
     responsiveClass: true,
     autoplay: false,
     nav: false,
-    autoHeight:true,
+    autoHeight:false,
     responsive: {
       0: {
         items: 1,
